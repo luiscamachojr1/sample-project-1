02f4289fd1b49d5445a8a0605f120511243dcb88
Luis Camacho <luiscamacho@MacBook-Pro.local>
2019-09-15 10:20:17 -0500

    day 2
    Worked on creating json file for the api, and listed items as a <ul>

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            items:["accounts"],
-            isLoaded: false
+            isLoaded: false,
+            items: []
         }
     }        
 
@@ -26,24 +26,27 @@ class App extends Component {
 
     render(){
 
-        var { isLoaded, items} = this.state;
+        //var { isLoaded, items} = this.state;
 
-        if(!isLoaded) {
+        if(false) {
             return <div>Loading...</div>
         }
 
         else {
 
-            return (
-                <div className="App">
+            console.log('this.state', Object.keys(this.state.items))
 
-                    <ul>
-                    <p>hi</p>
-                        {items.map(item => (
-                            <li key={item.id}>
-                                {item.name} | {item.accounts}
+            return (
+
+
+                <div className="App">
+                   <ul>
+                        {Object.keys(this.state.items).map(item => (
+                            <li>
+                                {item}:{JSON.stringify(this.state.items[item])}
                             </li>
-                        ))};
+                        ))}
+
                     </ul>
                     
                 </div>

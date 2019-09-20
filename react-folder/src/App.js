90598320ce62a59cc3bfaeb8db4bf5ba5eafb23d
Luis Camacho <luiscamacho@MacBook-Pro.local>
2019-09-20 02:10:11 -0500

    testing netlify

--- react-folder/src/App.js
+++ react-folder/src/App.js
@@ -1,65 +1,55 @@
 import React, {Component}from 'react';
-
 import './App.css';
-
-
 class App extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
+            items:["accounts"],
+            isLoaded: false
             isLoaded: false,
             items: []
         }
     }        
 
-    componentDidMount() {
-        fetch("https://api-v1.athletes.gg/users/7eaf20c8-ba0d-4570-a41d-f3087f6edbd0")
-            .then(res => res.json())
-            .then(json => {
-                this.setState({
-                    isLoaded: true,
-                    items: json, 
-                })
-            })
-    }
+@@ -26,24 +26,27 @@ class App extends Component {
 
     render(){
 
+        var { isLoaded, items} = this.state;
         //var { isLoaded, items} = this.state;
 
+        if(!isLoaded) {
         if(false) {
-
             return <div>Loading...</div>
         }
-
-        //items to grab: Gamer Tag, Name, AGG Score, About text, Avatar, cover image
 
         else {
 
             console.log('this.state', Object.keys(this.state.items))
 
             return (
+                <div className="App">
 
+                    <ul>
+                    <p>hi</p>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                {item.name} | {item.accounts}
 
                 <div className="App">
-                   
+                   <ul>
                         {Object.keys(this.state.items).map(item => (
                             <li>
-                                {item}:{JSON.stringify(
-                                    this.state.items[item]
-                                        )
-                                    }
-                                    
+                                {item}:{JSON.stringify(this.state.items[item])}
                             </li>
+                        ))};
                         ))}
 
-                    
-                    
+                    </ul>
+
                 </div>
             );
         }
     }
 }
-
-export default App;
+export default App; 

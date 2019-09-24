ef8c1cbc780051aee6ed4b1a54a545ee938e5433
Luis Camacho <luiscamacho@MacBook-Pro.local>
2019-09-23 20:49:11 -0500

    Parsed JSON
    Moving to Master

--- react-folder/src/info/infolist.js
+++ react-folder/src/info/infolist.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import DATA from '../data/nairo.json';
+import DataList from '../data/nairo.json';
 //import './App.css';
 
 
 class Profile extends Component {   
 	render(){ 
 	return(  
-		<div>
+		<div >
 		<h1>Hello World</h1>
-		
+		{DataList.map((bioDetails, index)=>{
+			return <h1> {bioDetails.about}</h1>
+		})}
 		</div>
 		);
     };
